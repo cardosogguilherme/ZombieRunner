5744253aa0035c101e7938f00c1cb3572a882696
Guilherme Cardoso <gcardoso@duck.com>
2024-07-08 19:44:53 +1200

    Weapon Zoom - Mouse Sensitivity

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
@@ -8,8 +9,17 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] Cinemachine.CinemachineVirtualCamera cinemachineCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = .5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     private bool zoomedInToogle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
@@ -18,14 +28,18 @@ public class WeaponZoom : MonoBehaviour
             if (!zoomedInToogle)
             {
                 zoomedInToogle = true;
-                fpsCamera.fieldOfView = zoomedInFOV;
+                // fpsCamera.fieldOfView = zoomedInFOV;
                 cinemachineCamera.m_Lens.FieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
             }
             else
             {
                 zoomedInToogle = false;
-                fpsCamera.fieldOfView = zoomedOutFOV;
+                // fpsCamera.fieldOfView = zoomedOutFOV;
                 cinemachineCamera.m_Lens.FieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
             }
         }
     }
